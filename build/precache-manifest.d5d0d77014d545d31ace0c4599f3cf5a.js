703e35e820f9dfe4446cdef1240b4ab5a5f47e94
Kiilas Kadri <kiilas@vdi-cubic-007.ad.helsinki.fi>
2020-04-30 17:30:37 +0300

    bla

---
+++ build/precache-manifest.d5d0d77014d545d31ace0c4599f3cf5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5405061a69f2d5001ca092a08dcb9cd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71ce0d39b67384205df",
+    "url": "/static/css/main.8ed0eddc.chunk.css"
+  },
+  {
+    "revision": "581509edc098e8f19e97",
+    "url": "/static/js/2.13a90021.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13a90021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71ce0d39b67384205df",
+    "url": "/static/js/main.0b26d653.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
