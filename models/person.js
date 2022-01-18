102a9a9c8390edefe441c07965f4f5f80c82b974
kkiilas <kadri.kiilas@helsinki.fi>
2022-01-18 21:58:34 +0200

    bla

--- models/person.js
+++ models/person.js
@@ -5,12 +5,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-})
+mongoose.connect(url)
   .then(result => {
     console.log('connected to MongoDB')
   })
