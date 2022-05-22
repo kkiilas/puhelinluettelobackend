4fc6e86e32c448cde89a1fb862978f5c7b91560b
kkiilas <kadri.kiilas@helsinki.fi>
2022-05-22 19:33:08 +0300

    uibuild

--- index.js
+++ index.js
@@ -20,14 +20,15 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
-const start = `<head>
-  <link
+const start = `
+  <head>
+    <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
       rel="stylesheet"
       integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
       crossorigin="anonymous"
-  />
-  <title>Puhelinluettelo - Info</title>
+    />
+    <title>Puhelinluettelo - Info</title>
   </head>
   <body class="container bg-dark text-light">`
 
